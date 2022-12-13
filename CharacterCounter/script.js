4cee34e36ec0f194220bf311ed0a8be13bf821e2
Promise Temitope <oluwatobilobatemi05@gmail.com>
2022-12-13 16:54:03 +0200

    Works.  Required button[Not dynamic]

--- CharacterCounter/script.js
+++ CharacterCounter/script.js
@@ -1,8 +1,13 @@
-var input= document.getElementById("input");
+var input = document.getElementById("input");
 var output = document.getElementById("output");
 var trigger = document.getElementById("count");
-var inputValues=[];
 
-function getCount(){
-    
+
+//Works with a button
+function getCount() {
+    var text = input.value;
+    var inputValues = [...text]
+
+
+    output.innerHTML = inputValues.length
 }
