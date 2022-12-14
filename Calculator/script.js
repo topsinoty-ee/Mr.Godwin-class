b4fe1c807c0769f6a1f40657a7a60f31d8805472
Promise Temitope <oluwatobilobatemi05@gmail.com>
2022-12-14 22:47:46 +0200

    Completed css

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,48 +1,29 @@
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+var button = document.getElementsByTagName("button");
 
+var buttonLength = button.length;
+for (let index = 0; index < buttonLength; index++) {
+    const selected = button[index]
+    selected.classList.add("button");
 
+    if (selected.innerHTML === "C") {
+        selected.classList.add("button");
+        selected.style.backgroundColor="orangered";
+    }
+    if (selected.innerHTML === "=") {
+        selected.classList.add("button");
+        selected.classList.add("fullGrid");
+        selected.style.backgroundColor="green";
+    }
+}
 
+input.type = "number";
+output.readOnly = true;
 
+function calculate() {
+    var inputValue = input.value;
+    var outputValue = output.value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-//EXPERIMENTS
-
-// var input = document.createElement("input");
-// var output = document.createElement("div");
-// var add = document.createElement("button");
-// var divide = document.createElement("button");
-// var root = document.createElement("button");
-
-
-
-// var documentFragment = document.createDocumentFragment();
-// let test = document.getElementById("test")
-
-// // documentFragment.appendChild(expo);
-
-// // var expo = document.createElement("select");
-// // var exponents = [2, 3, 4];
-// // var i = 0;
-// // for (i = 0; i < exponents.length; i++) {
-// //     var option = document.createElement("option");
-// //     option.value = exponents[i];
-// //     option.text = exponents[i];
-// //     expo.appendChild(option);
-// // }
-
-// add.innerHTML = "+";
-// divide.innerHTML = "/";
-// console.log("hello, world", add, divide, expo, root)
-
-
-// document.body.appendChild(documentFragment)
+    eval(inputValue)
+}
