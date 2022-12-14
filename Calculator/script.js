296afa2a6b32d4d4f8c1b6c7d3a0e2b0254fc5f7
Promise Temitope <oluwatobilobatemi05@gmail.com>
2022-12-14 12:41:16 +0200

    Experiment

---
+++ Calculator/script.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXPERIMENTS
+
+// var input = document.createElement("input");
+// var output = document.createElement("div");
+// var add = document.createElement("button");
+// var divide = document.createElement("button");
+// var root = document.createElement("button");
+
+
+
+// var documentFragment = document.createDocumentFragment();
+// let test = document.getElementById("test")
+
+// // documentFragment.appendChild(expo);
+
+// // var expo = document.createElement("select");
+// // var exponents = [2, 3, 4];
+// // var i = 0;
+// // for (i = 0; i < exponents.length; i++) {
+// //     var option = document.createElement("option");
+// //     option.value = exponents[i];
+// //     option.text = exponents[i];
+// //     expo.appendChild(option);
+// // }
+
+// add.innerHTML = "+";
+// divide.innerHTML = "/";
+// console.log("hello, world", add, divide, expo, root)
+
+
+// document.body.appendChild(documentFragment)
