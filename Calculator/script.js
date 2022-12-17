2997d92485b1ab947ab15dbaf90250808b044e09
Promise Temitope <oluwatobilobatemi05@gmail.com>
2022-12-18 00:33:09 +0200

    Calculator works properly

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,48 +1,50 @@
-var input = document.getElementById("input");
-var output = document.getElementById("output");
-var button = document.getElementsByTagName("button");
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+var button = document.getElementsByTagName('button');
 
 output.readOnly = true;
-
-function setCaretPosition(ctrl, pos) {
-    if (ctrl.setSelectionRange) {
-        ctrl.focus();
-        ctrl.setSelectionRange(pos, pos);
-
-    }
-}
-
-// Set the cursor position of the "#test-input" element to the end when the page loads
-
 
 var buttonLength = button.length;
 for (let index = 0; index < buttonLength; index++) {
     const selected = button[index]
-    selected.classList.add("button");
+    selected.classList.add('button');
 
-    if (selected.innerHTML === "C") {
-        selected.classList.add("button");
-        selected.style.backgroundColor = "orangered";
+    if (selected.innerHTML === 'C') {
+        selected.classList.add('button');
+        selected.style.backgroundColor = 'orangered';
     }
-    if (selected.innerHTML === "=") {
-        selected.classList.add("button");
-        selected.classList.add("fullGrid");
-        selected.style.backgroundColor = "green";
+    if (selected.innerHTML === '=') {
+        selected.classList.add('button');
+        selected.classList.add('fullGrid');
+        selected.style.backgroundColor = 'green';
     }
 }
 
-const isNumeric = int => /^[0-9^()√.*+/-]*$/gi.test(int); //Criteria for validation
-const alphaErr = "INPUT ONLY NUMBERS OR '-', '+', '/', '*' " //err message
+const isNumeric = int => /^[0-9^xsqrt.*+/√()-]*$/gi.test(int);       //Criteria for validation
+const alphaErr = "INPUT ONLY NUMBERS OR '-', '+', '/', '*' "    //err message
 
 function calculate() {
     var inValue = input.value;
-    const arr = [...inValue];
+
+    const replacer = (oldInval, newValue) => {
+        const includeCheck = () => inValue.includes(oldInval);
+        if (includeCheck(oldInval) === true) {
+            var newInval = inValue.replaceAll(oldInval, newValue);
+            inValue = newInval;
+        }
+    }
+
+    const sqrt = (x) => Math.sqrt(x);
+
+    replacer('x', '*');
+    replacer('^', '**');
 
 
     if (isNumeric(inValue) === false || !inValue) {
         alert(alphaErr);
     } else {
         while (isNumeric(inValue) === true) {
+            console.log(inValue)
             output.value = eval(inValue)
             break;
         }
@@ -50,18 +52,20 @@ function calculate() {
     console.log(isNumeric(inValue))
 }
 
+
+
 window.onclick = e => {
     var target = e.target.innerHTML
     console.log(target)
     if (target === '=') {
         calculate();
     } else if (target === 'x^2') {
-        input.value += '**2'
+        input.value += '^2'
     } else if (target === '√x') {
         input.value += 'sqrt()'
-        setCaretPosition(input, input.value.length - 1);
     } else if (target === 'C') {
-        input.value = "";
+        input.value = '';
+        output.value = ''
     } else (
         input.value += target
     )
