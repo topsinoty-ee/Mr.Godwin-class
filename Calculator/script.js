3325f1122431608400ce8d2124480a74bfa6f42a
Promise Temitope <oluwatobilobatemi05@gmail.com>
2022-12-18 01:08:05 +0200

    Commenting

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,13 @@
+//Get elements
 var input = document.getElementById('input');
 var output = document.getElementById('output');
 var button = document.getElementsByTagName('button');
 
+//set output type to readOnly
 output.readOnly = true;
 
+
+//Add coloring and numbering
 var buttonLength = button.length;
 for (let index = 0; index < buttonLength; index++) {
     const selected = button[index]
@@ -20,13 +24,18 @@ for (let index = 0; index < buttonLength; index++) {
     }
 }
 
+
 const isNumeric = int => /^[0-9^xsqrt.*+/√()-]*$/gi.test(int);       //Criteria for validation
 const alphaErr = "INPUT ONLY NUMBERS OR '-', '+', '/', '*' "    //err message
 
+
+//Calculate
 function calculate() {
+    //Make a value for input values
     var inValue = input.value;
 
-    const replacer = (oldInval, newValue) => {
+    //make a function that replaces the values since they cannot be converted directly
+    const converter = (oldInval, newValue) => {
         const includeCheck = () => inValue.includes(oldInval);
         if (includeCheck(oldInval) === true) {
             var newInval = inValue.replaceAll(oldInval, newValue);
@@ -34,12 +43,14 @@ function calculate() {
         }
     }
 
+    //the square root function
     const sqrt = (x) => Math.sqrt(x);
 
-    replacer('x', '*');
-    replacer('^', '**');
+    
+    converter('x', '*');    //This converts 'x' to '*'
+    converter('^', '**');   //This converts '^' to '**'
 
-
+    //Check validation
     if (isNumeric(inValue) === false || !inValue) {
         alert(alphaErr);
     } else {
@@ -55,13 +66,14 @@ function calculate() {
 
 
 
-// Attach the listener to the div element
+// Attach the listener to the calculator
 const calculator = document.querySelector('#calculator');
 
 calculator.addEventListener('click', (e) => {
     const target = e.target.innerHTML;
     const tagName = e.target.tagName;
 
+    //only work if tagName is button
     while (tagName === 'BUTTON') {
         if (target === '=') {
             calculate();
